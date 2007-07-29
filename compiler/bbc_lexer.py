bdbcdf343ec3625379559b61e605af58c3ddd8d6
robert.smallshire <robert.smallshire@localhost>
2007-07-29 13:14:54 +0000

    Better support for @%

--- compiler/bbc_lexer.py
+++ compiler/bbc_lexer.py
@@ -726,7 +726,7 @@ def t_ARRAYID_LPAREN(t):
 # TODO: Cannot use @ symbol at the beginning of
 #       any variable name. @% is a special variable
 def t_ID(t):
-    r'[@a-zA-Z_`][a-zA-Z_0-9`]*[$%&]?'
+    r'([a-zA-Z_`][a-zA-Z_0-9`]*[$%&]?)|@%'
     # Ampersand and hash suffixes only apply to BBC BASIC for Windows
     t.type = reserved.get(t.value, 'ID') # Check for reserved identifiers
     return t
