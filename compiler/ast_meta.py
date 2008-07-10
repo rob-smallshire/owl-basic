db106f9b039a81d83280cfbbac7e6c08afc68f87
robert.smallshire <robert.smallshire@localhost>
2008-07-10 15:14:01 +0000

    Added visitor support to AstNode and fixed initialization but in BPUT. Now requires IronPython 2

--- compiler/ast_meta.py
+++ compiler/ast_meta.py
@@ -133,6 +133,31 @@ class AstNode(object):
         return self._options
     
     options = property(_getOptions)
+    
+    def accept(self, visitor):
+        """
+        Accept method for visitor pattern.
+        """
+        return self._accept(self.__class__, visitor)
+    
+    def _accept(self, klass, visitor):
+        """
+        Recursive accept implementation that calls the right visitor
+        method 'overloaded' for the type of AstNode. This is done by
+        appending the class name to 'visit' so if the class name is AstNode
+        the method called is visitor.visitAstNode. If a method of that name
+        does not exist, then it recursively attempts to call the visitor
+        method on the superclass.
+        """
+        visitor_method = getattr(visitor, "visis%s" % klass.__name__, None)
+        if visitor_method is None:
+            bases = klass.__bases__
+            last = None
+            for i in bases:
+                last = self._accept(i, visitor)
+            return last
+        else:
+            return visitor_method(self)
         
 class Envelope(AstNode):
     n                 = Node(IntegerType)
@@ -153,7 +178,7 @@ class Envelope(AstNode):
 class Bput(AstNode):
     channel = Node(ChannelType)
     data    = Node(IntegerType)
-    newline = BoolOption(false)
+    newline = BoolOption(False)
 
 class Circle(AstNode):
     x_coord = Node(IntegerType)
