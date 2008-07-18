a19cccac3ff9a50759db1127d7c85e8d7faad138
i.smallshire <i.smallshire@localhost>
2008-07-18 22:29:53 +0000

    Updated filetype detection code. Dosnt work with different tokens from BB4W yet

--- compiler/Detoken.py
+++ compiler/Detoken.py
@@ -8,7 +8,9 @@
 # Some information obtained from source code from RISC OS Open.
 # v0.01 - first release.  Doesn't deal with GOTO line numbers.
 # v0.02 - edited to output line numbers where needed and fixed
-# the GOTO/RESTORE/GOSUB line numbers.
+#         the GOTO/RESTORE/GOSUB line numbers.
+# v0.03 - Added file type detection for input and provision
+#         for BB4W encoded tokens(NOT YET IMPLIMENTED)and line no's
 import struct, re, getopt, sys
 
 # The list of BBC BASIC V tokens:
@@ -75,7 +77,7 @@ def Detokenise(line):
                 #decode the 24 bit line number
                 return str(DecodeLineNo(token[1:]))
             else:
-                return tokens[tokenOrd-127]+ ' ' + token[1:]
+                return tokens[tokenOrd-127] + token[1:]
 
     # This regular expression is essentially:
     # (Optional extension token) followed by
@@ -102,41 +104,71 @@ def DecodeLineNo(lineNo):
 def ReadLines(data):
     """Returns a list of [line number, tokenised line] from a binary
        BBC BASIC V format file."""
+    fileType=0
+    if len(data) < 4:
+        # TODO unsure how you want to return error
+        raise Exception, "Bad Program"
+    
+    fileExt = data[-4:]
+    if fileExt[3] == '\x0d':
+        fileType=7 
+        decode = 0
+        lineEnd='\x0d'
+        print 'file uses CR'
+    if fileExt[3] == '\x0a':
+        fileType=6
+        decode = 0
+        lineEnd='\x0a'
+        print 'file uses LF'
+    if fileExt[2:4] == '\x0a\x0d':
+        fileType=5 
+        decode = 0
+        lineEnd='\x0a\x0d'
+        print 'file uses LFCR'
+    if fileExt[2:4] == '\x0d\x0a':
+        fileType=4 
+        deocde = 0
+        lineEnd='\x0d\x0a'
+        print 'file uses CRLF'
+    if fileExt[2:4] == '\x0d\xff':
+        fileType=2 
+        decode = 1
+        lineEnd='\x0d'
+        print 'BBC BASIC (Acorn)'
+    if fileExt == '\x0d\x00\xff\xff':
+        fileType=1
+        deocde = 2 
+        lineEnd='\x0d'
+        print 'BBC BASIC (80/86)'
+        
+    lenLineEnd = len(lineEnd)
     lines = []
-    start=0
-    lineNumberToken = True
-    skipTokens = False
     while True:
-        if len(data) < 2:
+        if (decode == 1) & (len(data) < 2):
+            raise Exception, "Bad program"
+        if (decode == 1) & (data[0] != '\r'):
+            raise Exception, "Bad program"
+        if (decode == 1) & (data[1] == '\xff'):
             break
-        if data[0:2] == '\r\xff': # EOF marker
-            break
-        if len(data) < 4:
-            break
-        # check if this line starts with a tokenised line number
-        # TODO sometimes gets it wrong with plane text
-        tmplen = ord(data[3])
-        if tmplen > len(data):
-            tmplen=len(data)-1
-        if (data[tmplen] == '\r') & (data[0] == '\r'):
-            #line seems to have a line number
+        if (decode == 2):
+            raise Exception, "BB4W tokens need testing"
+        if (decode == 1):
             lineNumber, length = struct.unpack('>hB', data[1:4])
             lineData = data[4:length]
-        else:
-            lineNumber = 0
-            length = data.find('\r',1)
-            #need a regular expression to determine what CRLF has been used
-            # start need to ne the position of the first char that is not CR or LF
-            start=0
-            if (data[0] == '\r') | (data[0] =='\n'):
-                start == 1
-                if (data[1] == '\r') | (data[1] == '\n'):
-                    start = 2
-            lineData = data[start:length]
-        print len(lineData),start
+        if (decode == 0):
+            lineNumber = ''
+            findStart=lenLineEnd
+            #make sure dont miss first chars of plane text
+            if data[0:lenLineEnd] != lineEnd:
+                findStart=0
+            length = data.find(lineEnd,findStart)
+            lineData = data[findStart:length]
         lines.append([lineNumber, lineData])
         data = data[length:]
-
+        if len(data) <= len(lineEnd):
+            # may need to check what data is in last chars
+            # all tests have been ending tokens/CR/LF
+            break
     return lines
 
 def Decode(data, output):
@@ -149,16 +181,16 @@ def Decode(data, output):
         lineData = Detokenise(line)
         if lineNoNeeded:
             output.write(str(lineNumber))
-        output.write(lineData + '\n')
+        output.write(lineData + '\n') 
 
 if __name__ == "__main__":
     optlist, args = getopt.getopt(sys.argv[1:], '')
     if len(args) != 2:
         print "Usage: %s INPUT OUTPUT" % sys.argv[0]
         #sys.exit(1)
-    entireFile = open(args[0], 'rb').read()
-    output = open(args[1], 'w')
-    #entireFile = open('calc.txt', 'rb').read()
-    #output = open('calc1.txt', 'w')
+    #entireFile = open(args[0], 'rb').read()
+    #output = open(args[1], 'w')
+    entireFile = open('calc.bbc', 'rb').read()
+    output = open('calc1.txt', 'w')
     Decode(entireFile, output)
     output.close()
