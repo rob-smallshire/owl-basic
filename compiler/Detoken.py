85ffe26fe8afd73070d55690f961287a7363e36b
i.smallshire <i.smallshire@localhost>
2008-07-19 03:00:40 +0000

    Now supports Acorn and BB4W Token formats.(Autodetect file format).....Needs to be tested with BB4W line numbers (GOTO/RESTORE)

--- compiler/Detoken.py
+++ compiler/Detoken.py
@@ -10,7 +10,8 @@
 # v0.02 - edited to output line numbers where needed and fixed
 #         the GOTO/RESTORE/GOSUB line numbers.
 # v0.03 - Added file type detection for input and provision
-#         for BB4W encoded tokens(NOT YET IMPLIMENTED)and line no's
+#         for BB4W encoded tokens
+# v0.04 - Now decodes BB4W tokens as well as Acorn.
 import struct, re, getopt, sys
 
 # The list of BBC BASIC V tokens:
@@ -19,27 +20,19 @@ tokens = [
     'OTHERWISE', # 7f
     'AND', 'DIV', 'EOR', 'MOD', 'OR', 'ERROR', 'LINE', 'OFF',
     'STEP', 'SPC', 'TAB(', 'ELSE', 'THEN', '<line>' , 'OPENIN', 'PTR',
-
     'PAGE', 'TIME', 'LOMEM', 'HIMEM', 'ABS', 'ACS', 'ADVAL', 'ASC',
     'ASN', 'ATN', 'BGET', 'COS', 'COUNT', 'DEG', 'ERL', 'ERR',
-
     'EVAL', 'EXP', 'EXT', 'FALSE', 'FN', 'GET', 'INKEY', 'INSTR(',
     'INT', 'LEN', 'LN', 'LOG', 'NOT', 'OPENUP', 'OPENOUT', 'PI',
-
     'POINT(', 'POS', 'RAD', 'RND', 'SGN', 'SIN', 'SQR', 'TAN',
     'TO', 'TRUE', 'USR', 'VAL', 'VPOS', 'CHR$', 'GET$', 'INKEY$',
-
     'LEFT$(', 'MID$(', 'RIGHT$(', 'STR$', 'STRING$(', 'EOF',
-        '<ESCFN>', '<ESCCOM>', '<ESCSTMT>',
-    'WHEN', 'OF', 'ENDCASE', 'ELSE' # ELSE2
-        , 'ENDIF', 'ENDWHILE', 'PTR',
-
+    '<ESCFN>', '<ESCCOM>', '<ESCSTMT>',
+    'WHEN', 'OF', 'ENDCASE', 'ELSE', 'ENDIF', 'ENDWHILE', 'PTR',
     'PAGE', 'TIME', 'LOMEM', 'HIMEM', 'SOUND', 'BPUT', 'CALL', 'CHAIN',
     'CLEAR', 'CLOSE', 'CLG', 'CLS', 'DATA', 'DEF', 'DIM', 'DRAW',
-
     'END', 'ENDPROC', 'ENVELOPE', 'FOR', 'GOSUB', 'GOTO', 'GCOL', 'IF',
     'INPUT', 'LET', 'LOCAL', 'MODE', 'MOVE', 'NEXT', 'ON', 'VDU',
-
     'PLOT', 'PRINT', 'PROC', 'READ', 'REM', 'REPEAT', 'REPORT', 'RESTORE',
     'RETURN', 'RUN', 'STOP', 'COLOUR', 'TRACE', 'UNTIL', 'WIDTH', 'OSCLI']
 
@@ -48,7 +41,7 @@ cfnTokens = [
     'SUM', 'BEAT']
 # Referred to as "ESCCOM" tokens in the source, starting at 0x8e.
 comTokens = [
-    'APPEND', 'AUTO', 'CRUNCH', 'DELET', 'EDIT', 'HELP', 'LIST', 'LOAD',
+    'APPEND', 'AUTO', 'CRUNCH', 'DELETE', 'EDIT', 'HELP', 'LIST', 'LOAD',
     'LVAR', 'NEW', 'OLD', 'RENUMBER', 'SAVE', 'TEXTLOAD', 'TEXTSAVE', 'TWIN'
     'TWINO', 'INSTALL']
 # Referred to as "ESCSTMT", starting at 0x8e.
@@ -56,8 +49,28 @@ stmtTokens= [
     'CASE', 'CIRCLE', 'FILL', 'ORIGIN', 'PSET', 'RECT', 'SWAP', 'WHILE',
     'WAIT', 'MOUSE', 'QUIT', 'SYS', 'INSTALL', 'LIBRARY', 'TINT', 'ELLIPSE',
     'BEATS', 'TEMPO', 'VOICES', 'VOICE', 'STEREO', 'OVERLAY']
+# BB4W tokens....
+# these tokens start at 128 and wrap around to 0-15
+bb4wTokens=["AND","DIV","EOR","MOD","OR","ERROR","LINE","OFF",
+        "STEP","SPC","TAB(","ELSE","THEN","","OPENIN","PTR",
+        "PAGE","TIME","LOMEM","HIMEM","ABS","ACS","ADVAL","ASC",
+        "ASN","ATN","BGET","COS","COUNT","DEG","ERL","ERR",
+        "EVAL","EXP","EXT","FALSE","FN","GET","INKEY","INSTR(",
+        "INT","LEN","LN","LOG","NOT","OPENUP","OPENOUT","PI",
+        "POINT(","POS","RAD","RND","SGN","SIN","SQR","TAN",
+        "TO","TRUE","USR","VAL","VPOS","CHR$","GET$","INKEY$",
+        "LEFT$(","MID$(","RIGHT$(","STR$","STRING$(","EOF","SUM","WHILE",
+        "CASE","WHEN","OF","ENDCASE","OTHERWISE","ENDIF","ENDWHILE","PTR",
+        "PAGE","TIME","LOMEM","HIMEM","SOUND","BPUT","CALL","CHAIN",
+        "CLEAR","CLOSE","CLG","CLS","DATA","DEF","DIM","DRAW",
+        "END","ENDPROC","ENVELOPE","FOR","GOSUB","GOTO","GCOL","IF",
+        "INPUT","LET","LOCAL","MODE","MOVE","NEXT","ON","VDU",
+        "PLOT","PRINT","PROC","READ","REM","REPEAT","REPORT","RESTORE",
+        "RETURN","RUN","STOP","COLOUR","TRACE","UNTIL","WIDTH","OSCLI",
+        "","CIRCLE","ELLIPSE","FILL","MOUSE","ORIGIN","QUIT","RECTANGLE",
+        "SWAP","SYS","TINT","WAIT","INSTALL","","PRIVATE","BY","EXIT"]
 
-def Detokenise(line):
+def Detokenise(line, decode):
     """Replace all tokens in the line 'line' with their ASCII equivalent."""
     # Internal function used as a callback to the regular expression
     # to replace tokens with their ASCII equivalents.
@@ -78,32 +91,54 @@ def Detokenise(line):
                 return str(DecodeLineNo(token[1:]))
             else:
                 return tokens[tokenOrd-127] + token[1:]
+    def ReplaceFuncBB4W(match):
+        token = match.groups()
+        token=token[0]
+        tokenOrd = ord(token[0])
+        if token[0] == '\x8d': # line number following token
+            #decode the 24 bit line number
+            return str(DecodeLineNo(token[1:]))
+        else:
+            return bb4wTokens[tokenOrd ^ 128] + token[1:]
 
-    # This regular expression is essentially:
-    # (Optional extension token) followed by
-    # (REM token followed by the rest of the line)
-    #     -- this ensures we don't detokenise the REM statement itself
-    # OR
-    # (Line number following token, with 3 characters in the range 64-127)
-    # OR
-    # (any token)
-    return re.sub(r'([\xc6-\xc8])?(\xf4.*|\x8d[\x40-\x7f]{3}|[\x7f-\xff])', ReplaceFunc, line)
+    if decode ==2:
+        # This uses BB4W encoding
+        # This regular expression is essentially:
+        # (REM token followed by the rest of the line)
+        #     -- this ensures we don't detokenise the REM statement itself
+        # OR
+        # (Line number following token, with 3 characters in the range 64-127)
+        # OR
+        # (any token 127-255)
+        # OR
+        # (any token 0-15)
+        return re.sub(r'(\xf4.*|\x8d[\x40-\x7f]{3}|[\x7f-\xff]|[\x00-\x0f])', ReplaceFuncBB4W, line)
+    else:   
+        # Acorn encoding
+        # This regular expression is essentially:
+        # (Optional extension token) followed by
+        # (REM token followed by the rest of the line)
+        #     -- this ensures we don't detokenise the REM statement itself
+        # OR
+        # (Line number following token, with 3 characters in the range 64-127)
+        # OR
+        # (any token)
+        return re.sub(r'([\xc6-\xc8])?(\xf4.*|\x8d[\x40-\x7f]{3}|[\x7f-\xff])', ReplaceFunc, line)
 
 def DecodeLineNo(lineNo):
     """Returns a line number from a 24bit encoded line number"""
-    # TODO could be edited to also convert 16 bit line numbers depending on input string length
     byte0=ord(lineNo[0])
     byte1=ord(lineNo[1])
     byte2=ord(lineNo[2])
     #needed to be ANDed with 255 after multipy because with this formula
-    #on the 6502 it lost the high bits with the Logical Shift 
+    #on the 6502 it moved the high bits to carry with the Logical Shift 
     msb = byte2 ^ (( byte0 * 16) & 255)          
     lsb = byte1 ^ (((byte0 & 0x30 ) * 4) & 255)  
     return (lsb + (msb * 256))
 
 def ReadLines(data):
     """Returns a list of [line number, tokenised line] from a binary
-       BBC BASIC V format file."""
+       BBC BASIC format file."""
     fileType=0
     if len(data) < 4:
         # TODO unsure how you want to return error
@@ -114,34 +149,36 @@ def ReadLines(data):
         fileType=7 
         decode = 0
         lineEnd='\x0d'
-        print 'file uses CR'
+        fileTypeName = 'plain text CR'
     if fileExt[3] == '\x0a':
         fileType=6
         decode = 0
         lineEnd='\x0a'
-        print 'file uses LF'
+        fileTypeName = 'plain text LF'
     if fileExt[2:4] == '\x0a\x0d':
         fileType=5 
         decode = 0
         lineEnd='\x0a\x0d'
-        print 'file uses LFCR'
+        fileTypeName = 'plain text LFCR'
     if fileExt[2:4] == '\x0d\x0a':
         fileType=4 
         deocde = 0
         lineEnd='\x0d\x0a'
-        print 'file uses CRLF'
+        fileTypeName = 'plain text CRLF'
     if fileExt[2:4] == '\x0d\xff':
         fileType=2 
         decode = 1
         lineEnd='\x0d'
-        print 'BBC BASIC (Acorn)'
+        fileTypeName = 'BBC BASIC (Acorn)'
     if fileExt == '\x0d\x00\xff\xff':
         fileType=1
-        deocde = 2 
+        decode = 2 
         lineEnd='\x0d'
-        print 'BBC BASIC (80/86)'
-        
+        fileTypeName = 'BBC BASIC (80/86)'
+    
+    print fileTypeName
     lenLineEnd = len(lineEnd)
+    
     lines = []
     while True:
         if (decode == 1) & (len(data) < 2):
@@ -151,11 +188,23 @@ def ReadLines(data):
         if (decode == 1) & (data[1] == '\xff'):
             break
         if (decode == 2):
-            raise Exception, "BB4W tokens need testing"
+            # TODO this needs testing 
+            # i have read somewhere that bb4w uses different tokens
+            # and also has diff line number formatting
+            # (http://bb4w.wikispaces.com/Format)
+            # {<len> <linelo> <linehi> <text> <cr>} <00> <ff> <ff>
+
+            lineNumber, length= struct.unpack('>hB', data[2]+data[1]+data[0]) # line number bytes in different order
+            if lineNumber == -1:
+                break
+            lineData = data[3:length]
         if (decode == 1):
+            #  {<cr> <linehi> <linelo> <len> <text>} <cr> <ff>
             lineNumber, length = struct.unpack('>hB', data[1:4])
             lineData = data[4:length]
         if (decode == 0):
+            #  {[<text>] [<cr>|<lf>|<cr><lf>|<lf><cr>]}
+            
             lineNumber = ''
             findStart=lenLineEnd
             #make sure dont miss first chars of plane text
@@ -163,7 +212,7 @@ def ReadLines(data):
                 findStart=0
             length = data.find(lineEnd,findStart)
             lineData = data[findStart:length]
-        lines.append([lineNumber, lineData])
+        lines.append([lineNumber, Detokenise(lineData, decode)])
         data = data[length:]
         if len(data) <= len(lineEnd):
             # may need to check what data is in last chars
@@ -177,8 +226,7 @@ def Decode(data, output):
     if None != re.search('\x8d', data): #check if line number references are used anywhere in the file
         lineNoNeeded = True
     lines = ReadLines(data)
-    for lineNumber, line in lines:
-        lineData = Detokenise(line)
+    for lineNumber, lineData in lines:
         if lineNoNeeded:
             output.write(str(lineNumber))
         output.write(lineData + '\n') 
@@ -187,10 +235,11 @@ if __name__ == "__main__":
     optlist, args = getopt.getopt(sys.argv[1:], '')
     if len(args) != 2:
         print "Usage: %s INPUT OUTPUT" % sys.argv[0]
-        #sys.exit(1)
-    #entireFile = open(args[0], 'rb').read()
-    #output = open(args[1], 'w')
-    entireFile = open('calc.bbc', 'rb').read()
-    output = open('calc1.txt', 'w')
+        sys.exit(1)
+    entireFile = open(args[0], 'rb').read()
+    output = open(args[1], 'w')
+
+    #entireFile = open('sudoku.bbc', 'rb').read()
+    #output = open('calc1.txt', 'w')
     Decode(entireFile, output)
     output.close()
