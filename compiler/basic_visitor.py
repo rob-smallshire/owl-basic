607b94c8a49a9e59a2a245c68f252f7467d15c77
robert.smallshire <robert.smallshire@localhost>
2008-09-07 19:56:39 +0000

    BasicVisitor MID$ and STR$ (untested)

--- compiler/basic_visitor.py
+++ compiler/basic_visitor.py
@@ -20,3 +20,20 @@ class BasicVisitor(Visitor):
         sys.stdout.write(", ")
         self.visit(circle.radius)
         sys.stdout.write('\n')
+        
+    def visitMidStrFunc(self, mid_str_func):
+        sys.stdout.write("MID$(")
+        self.visit(mid_str_func.source)
+        sys.stdout.write(", ")
+        self.visit(mid_str_func.position)
+        if mid_str_func.length is not None:
+            sys.stdout.write(", ")
+            self.visit(mid_str_func.length)
+        sys.stdout.write(")")
+        
+    def visitStrStringFunc(self, str_string_func):
+        sys.stdout.write("STR$")
+        if str_string_func.base == 16:
+            sys.stdout.write("~")
+        self.visit(str_string_func.factor)
+        
