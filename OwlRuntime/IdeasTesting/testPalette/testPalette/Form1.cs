37ea9a065c719728324440ef0ae254818778e3e8
robert.smallshire <robert.smallshire@localhost>
2009-06-20 14:19:42 +0000

    Code style improvements

--- OwlRuntime/IdeasTesting/testPalette/testPalette/Form1.cs
+++ OwlRuntime/IdeasTesting/testPalette/testPalette/Form1.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using System.Drawing.Imaging;
 
@@ -15,8 +11,8 @@ namespace testPalette
 {
     public partial class Form1 : Form
     {
-        private System.Drawing.Bitmap myBitmap; // Our Bitmap declaration
-        private System.Drawing.Bitmap indexedBitmap;
+        private Bitmap myBitmap; // Our Bitmap declaration
+        private Bitmap indexedBitmap;
 
 
         public Form1()
@@ -27,9 +23,8 @@ namespace testPalette
         private void Form1_Load(object sender, EventArgs e)
         {
             // draw red circles on the 
-            Graphics graphicsObj;
-            myBitmap = new Bitmap(this.ClientRectangle.Width, this.ClientRectangle.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-            indexedBitmap = new Bitmap(myBitmap.Width, myBitmap.Height, System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
+            myBitmap = new Bitmap(ClientRectangle.Width, ClientRectangle.Height, PixelFormat.Format24bppRgb);
+            indexedBitmap = new Bitmap(myBitmap.Width, myBitmap.Height, PixelFormat.Format8bppIndexed);
 
             // Set the palette
             ColorPalette pal = indexedBitmap.Palette;
@@ -44,35 +39,26 @@ namespace testPalette
             //indexedBitmap.Palette.Entries[0] = Color.FromArgb(0, 0, 0);
             
             //cleat the graphics object to logical colour 0
-            graphicsObj = Graphics.FromImage(myBitmap);
-            graphicsObj.Clear(System.Drawing.Color.FromArgb(0, 0, 0));
+            Graphics graphicsObj = Graphics.FromImage(myBitmap);
+            graphicsObj.Clear(Color.FromArgb(0, 0, 0));
             
-            for (int i = 0; i <= 255; i++)
+            for (int i = 0; i <= 255; ++i)
             {
-                Pen myPen = new Pen(System.Drawing.Color.FromArgb(0, 0, i), 3);
+                Pen myPen = new Pen(Color.FromArgb(0, 0, i), 3);
                 graphicsObj.DrawEllipse(myPen, new Rectangle(0 + i, 0 + i, 512-(2*i), 512-(2*i)));
             }
             graphicsObj.Dispose();
 
-            indexedFromBlueBitmap(myBitmap, indexedBitmap, 8);
-
-
-
+            IndexedFromBlueBitmap(myBitmap, indexedBitmap, 8);
         }
-
-
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Graphics graphicsObj = e.Graphics;
-
-             //data for the indexed bitmap
-
+            //data for the indexed bitmap
             e.Graphics.DrawImage(indexedBitmap, 0, 0);
-        
         }
 
-        unsafe private void indexedFromBlueBitmap(Bitmap sourceBitmap, Bitmap destBitmap, byte destBpp)
+        static private void IndexedFromBlueBitmap(Bitmap sourceBitmap, Bitmap destBitmap, byte destBpp)
         {
             PixelFormat bppFormat;
             switch (destBpp)
@@ -95,37 +81,49 @@ namespace testPalette
                     throw new ArgumentOutOfRangeException();
             }
 
-            int mask = (1 << destBpp)-1;
-            int notMask = 255 - mask;
-
-            BitmapData indexedBitmapData = destBitmap.LockBits(
-                new Rectangle(0, 0, destBitmap.Width, destBitmap.Height),
-                ImageLockMode.ReadWrite, bppFormat);
-
-            // data for the orig bitmap
-            BitmapData sourceBitmapData = sourceBitmap.LockBits(
-                new Rectangle(0, 0, sourceBitmap.Width, sourceBitmap.Height),
-                ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
-            // size of the source pixels in bytes
-            int sourcePixelSize = 3;
-
-            for (int y = 0; y < sourceBitmapData.Height; y++)
+            BitmapData indexedBitmapData = null;
+            try
             {
+                Debug.Assert(destBitmap.Height == sourceBitmap.Height);
+                Debug.Assert(destBitmap.Width == sourceBitmap.Width);
+                Rectangle rectangle = new Rectangle(0, 0, sourceBitmap.Width, sourceBitmap.Height);
+                indexedBitmapData = destBitmap.LockBits(
+                    rectangle,
+                    ImageLockMode.ReadWrite, bppFormat);
 
-                byte* sourceRow = (byte*)sourceBitmapData.Scan0 + (y * sourceBitmapData.Stride);
-                byte* destRow = (byte*)indexedBitmapData.Scan0 + (y * indexedBitmapData.Stride);
-
-                for (int x = 0; x < sourceBitmapData.Width; x++)
+                BitmapData sourceBitmapData = null;
+                try
                 {
-                    int origpixel = destRow[x] & notMask;
-                    int sourcepixel = (sourceRow[x * sourcePixelSize] & mask);
-                    destRow[x] = (byte)(origpixel | sourcepixel);
+                    // data for the orig bitmap
+                    sourceBitmapData = sourceBitmap.LockBits(
+                        rectangle,
+                        ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+                    // size of the source pixels in bytes
+                    const int sourcePixelSize = 3;
 
+                    for (int y = 0; y < sourceBitmapData.Height; ++y)
+                    {
+                        unsafe
+                        {
+                            byte* sourceRow = (byte*) sourceBitmapData.Scan0 + (y * sourceBitmapData.Stride);
+                            byte* destRow = (byte*) indexedBitmapData.Scan0 + (y * indexedBitmapData.Stride);
+
+                            for (int x = 0; x < sourceBitmapData.Width; ++x)
+                            {
+                                destRow[x] = sourceRow[x * sourcePixelSize];
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    sourceBitmap.UnlockBits(sourceBitmapData);      
                 }
             }
-
-            destBitmap.UnlockBits(indexedBitmapData);
-            sourceBitmap.UnlockBits(sourceBitmapData);
+            finally
+            {
+                destBitmap.UnlockBits(indexedBitmapData);
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -139,9 +137,7 @@ namespace testPalette
             }
             pal.Entries[pal.Entries.Length - 1] = tmp;
             indexedBitmap.Palette = pal;
-            this.Invalidate();
+            Invalidate();
         }
-
-
     }
 }
