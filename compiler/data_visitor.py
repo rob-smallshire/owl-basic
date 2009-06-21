f4e8eebac82e47b750ddb840eefa09df3474b204
robert.smallshire <robert.smallshire@localhost>
2009-06-21 20:41:43 +0000

    DataVisitor for extracting data from DATA statements

---
+++ compiler/data_visitor.py
@@ -0,0 +1,34 @@
+import logging
+import re
+from visitor import Visitor
+
+class DataVisitor(Visitor):
+    def __init__(self):
+        self.data = []
+        self.index = {} # line number -> data[index]
+
+    def parse(self, data):
+        "Parse the text following a DATA statement into items"
+        # Break the data into fields
+        raw_items = re.findall(r'(?:\s*"((?:[^"]+|"")*)"(?!")\s*)|([^,]+)', data)
+        items = []
+        for i, (quoted, unquoted) in enumerate(raw_items):
+            if quoted:
+                item = quoted.replace('""', '"')
+            else:
+                item = unquoted.lstrip()
+                # If its the last item on the line, strip trailing space
+                if i == len(raw_items) - 1:
+                    item = item.rstrip()
+            items.append(item)
+        return items
+    
+    def visitAstNode(self, node):
+        node.forEachChild(self.visit)
+    
+    def visitData(self, statement):
+       logging.debug("DATA statement : %s" % statement.data)
+       self.index[statement.lineNum] = len(self.data)
+       items = self.parse(statement.data)
+       self.data.extend(items)
+       
