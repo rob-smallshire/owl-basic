6fa19a8c9849b7f81d194e7090192b2510d5eb20
i.smallshire <i.smallshire@localhost>
2009-06-24 21:03:07 +0000

    IANDebugApp added

---
+++ OwlRuntime/IanDebugApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OwlRuntime.platform.riscos;
+
+namespace IanDebugApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VduSystemTest tst = new VduSystemTest();
+            //tst.Test25();
+            tst.TestPalette();
+            tst.TestPaletteWheel();
+        }
+    }
+}
